84756ac67d57d6a801bb2e093bc60bc215d70803
Pablo Martínez <martipev2@hotmail.com>
2018-11-22 22:29:15 -0500

    Cambios para que express jale

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const routesStudent = require('./routes/student');
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'web/build')));
 app.use(bodyParser.json());
 app.use('/student', routesStudent);
 
 var port = process.env.PORT || 3001;
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/build/build/index.html'));
+});
 
 app.listen(port, () => console.log('App listening on port ' + port + '!'))
