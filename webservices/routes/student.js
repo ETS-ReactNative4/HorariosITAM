5aa039aa3d6844aa009ba43bcb6d439d300bc417
Pablo Martínez <martipev2@hotmail.com>
2018-11-22 21:25:42 -0500

    Ready for awesome

---
+++ webservices/routes/student.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router();
+var pgp = require('pg-promise')({
+  /* initialization options */
+  capSQL: true // capitalize all generated SQL
+});
+
+var database_url = 'postgres://yjxmxmdcuhhkqm:3ace012f4774e98479d3ad0819355992a136834c24a8010feeed1f682fddb20a@ec2-54-83-8-246.compute-1.amazonaws.com:5432/db6fg3ucd43k2l';
+const db = pgp(database_url);
+
+router.get('/ping', function (req, res){
+  res.status(200).send('pong');
+});
+
+
+module.exports = router
