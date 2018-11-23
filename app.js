0e474d707161b95566356a54c3b085ce48d817c6
Pablo Martínez <martipev2@hotmail.com>
2018-11-22 22:32:56 -0500

    Path agregado

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const routesStudent = require('./routes/student');
 
