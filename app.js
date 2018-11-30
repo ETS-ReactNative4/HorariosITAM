a8aece40fb4447b4205e9d5e8396ce07466f6c46
Pablo Martínez <martipev2@hotmail.com>
2018-11-29 21:28:12 -0500

    app?

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ app.use('/student', routesStudent);
 
 var port = process.env.PORT || 3001;
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/build/build/index.html'));
-});
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 
 app.listen(port, () => console.log('App listening on port ' + port + '!'))
