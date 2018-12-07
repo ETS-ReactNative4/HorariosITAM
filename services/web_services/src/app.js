1408773ade51ab86ca7e08d8bccfafe843f29a68
Pablo Martínez <martipev2@hotmail.com>
2018-12-07 00:22:36 -0500

    Login funcional

--- services/web_services/src/app.js
+++ services/web_services/src/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routesStudent = require('./routes/student');
 
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
